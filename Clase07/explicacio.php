521d3d3971a23dd89ca2c221bd6769466224d25a
maxiVidarte <maxiavidarte@gmail.com>
2017-10-24 22:07:40 -0300

    clase 07

---
+++ Clase07/explicacio.php
@@ -0,0 +1,7 @@
+<?php
+//en el archivo datos va a estar todo relacionado con la base de datos y archivos...
+
+//ejemplo personaDatos ABML "guardar()"
+//validaciones negocio y datos del lado backend
+
+?>
