c86231d9de4945a5055aedcd902500c5aeef5f0c
maxiVidarte <maxiavidarte@gmai.com>
2017-12-05 01:16:57 -0300

    ya no se entiende nada

--- PrimerParcialPractica/AltaVenta.php
+++ PrimerParcialPractica/AltaVenta.php
@@ -1,6 +1,16 @@
 <?php 
-include_once "Clases/Ventas.php";
 include_once "Clases/Helado.php";
 
+$usuario = $_POST["usuario"];
+$sabor = $_POST["sabor"];
+$tipo = $_POST["tipo"];
+$cantidad = $_POST["cantidad"];
+
+if(Helado::VentaCarga($usuario,$sabor,$tipo,$cantidad)){
+    echo "cargo";
+}
+else{
+    echo "no cargo";
+}
 
 ?>
--- PrimerParcialPractica/Clases/Helado.php
+++ PrimerParcialPractica/Clases/Helado.php
@@ -13,10 +13,46 @@ class Helado{
             $this->tipo = $tipo;
             $this->cantidad = $cantidad;
         }
-    }
+    }   
     
     public function ToString(){
         return $this->sabor."-".$this->precio."-".$this->tipo."-".$this->cantidad."\n";
+    }
+    public static function TraerTodosLosHelados()
+	{
+		$ListaDeHeladosLeidos = array();
+		
+		$archivo=fopen("archivos/Helados.txt", "r");
+		
+		while(!feof($archivo))
+		{
+			$archAux = fgets($archivo);
+			$helados = explode("-", $archAux);
+			$helados[0] = trim($helados[0]);
+			if($helados[0] != ""){
+				$ListaDeHeladosLeidos[] = new Helado($helados[0], $helados[1],$helados[2],$helados[3]);
+			}
+		}
+		fclose($archivo);
+		return $ListaDeHeladosLeidos;
+    }
+    public static function TraerTodasLasVentas()
+	{
+		$ListaDeVentasLeidos = array();
+		
+		$archivo=fopen("archivos/Ventas.txt", "r");
+		
+		while(!feof($archivo))
+		{
+			$archAux = fgets($archivo);
+			$helados = explode("-", $archAux);
+			$helados[0] = trim($helados[0]);
+			if($helados[0] != ""){
+				$ListaDeVentasLeidos[] = ($helados[0]."-".$helados[1]."-".$helados[2]."-".$helados[3]);
+			}
+		}
+		fclose($archivo);
+		return $ListaDeVentasLeidos;
     }
     public function HeladoEsta($sabor,$tipo){
         $archivo = fopen("archivos/Helados.txt","r");
@@ -39,16 +75,35 @@ class Helado{
         return false;
     }
     public static function Guardar($objeto){
-        if(!$objeto->HeladoEsta($this->sabor,$this->tipo)){
         $archivo = fopen("archivos/Helados.txt","a");
         fwrite($archivo,$objeto->ToString());
         fclose($archivo);
         return true;
-        }
-        return false;
     }
-    
+    public static function VentaCarga($usuario,$sabor,$tipo,$cantidad){
+         $miArray= Helado::TraerTodosLosHelados();
+         $cambio = false;
+         foreach ($miArray as $key ) {
+             if($key->sabor == $sabor && $key->tipo == $tipo)
+                if($key->cantidad >= $cantidad){
+                    $key->cantidad = $key->cantidad -$cantidad;
+                    $cambio  = true;
+                }
+        }      
+        var_dump($cambio);
+        var_dump($miArray);
+        if($cambio){
+        $archivo =fopen("archivos/Helados.txt","w");
+            foreach ($miArray as $key) {
+                var_dump($key->toString());
+            }   
+            fclose($archivo);
+        }
+        return true;
+
+     }
 }
+
 
 
 
